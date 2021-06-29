d47bf6a0a00a52d90bda8f5649d68375dccd6688
Stepan <bai_777@mail.ru>
2021-06-29 15:16:21 +0300

    Создал Activity для портретного отображения заметок и даты

---
+++ app/src/main/java/com/example/notes/DisplayingTheDescriptionOfNotes.java
@@ -0,0 +1,20 @@
+package com.example.notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+public class DisplayingTheDescriptionOfNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_displaying_the_description_of_notes);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            finish();
+            return;
+        }
+    }
+}
