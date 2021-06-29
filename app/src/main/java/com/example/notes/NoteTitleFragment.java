a6de1aac321549cb2e7a632ef43bcb36289d405a
Stepan <bai_777@mail.ru>
2021-06-29 14:11:21 +0300

    убрал лишнее из NoteTitleFragment extends Fragment, реализовал метод initList(View view)

--- app/src/main/java/com/example/notes/NoteTitleFragment.java
+++ app/src/main/java/com/example/notes/NoteTitleFragment.java
@@ -2,63 +2,43 @@ package com.example.notes;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NoteTitleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class NoteTitleFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public NoteTitleFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NoteTitleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NoteTitleFragment newInstance(String param1, String param2) {
-        NoteTitleFragment fragment = new NoteTitleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_note_title, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+    private void initList(View view) {
+        LinearLayout linearLayout = (LinearLayout)view;
+        String[] notesTitle = getResources().getStringArray(R.array.note_title);
+        for (int i = 0; i < notesTitle.length; i++) {
+            String notes = notesTitle[i];
+            TextView textViewNotesTitle = new TextView(getContext());
+            textViewNotesTitle.setText(notes);
+            textViewNotesTitle.setTextSize(20);
+            linearLayout.addView(textViewNotesTitle);
+        }
     }
 }
