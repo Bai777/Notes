c2e99e2e455266863d9fc4f8b22e6dcdbe243337
Stepan <bai_777@mail.ru>
2021-06-30 14:43:37 +0300

    Сделал добавление даты через DatePicker

--- app/src/main/java/com/example/notes/NoteDescriptionFragment.java
+++ app/src/main/java/com/example/notes/NoteDescriptionFragment.java
@@ -9,12 +9,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 
 public class NoteDescriptionFragment extends Fragment {
 
     public static final String ARG_INDEX = "index";
     private int index;
+    private DatePicker datePicker;
+    private AppCompatTextView textViewNoteCreateData;
 
 
     public static NoteDescriptionFragment newInstance(int index) {
@@ -37,13 +42,31 @@ public class NoteDescriptionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view =  inflater.inflate(R.layout.fragment_note_description, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_note_description, container, false);
         AppCompatTextView textViewNoteDescription = view.findViewById(R.id.textViewNoteDescription);
-        AppCompatTextView textViewNoteCreateData = view.findViewById(R.id.textViewNoteCreateData);
+        textViewNoteCreateData = view.findViewById(R.id.textViewNoteCreateData);
         String[] noteDescription = getResources().getStringArray(R.array.note_description);
         String[] noteCreateData = getResources().getStringArray(R.array.date_the_note_was_created);
         textViewNoteDescription.setText(noteDescription[index]);
-        textViewNoteCreateData.setText(noteCreateData[index]);
+        //textViewNoteCreateData.setText(noteCreateData[index]);
+
+        datePicker = view.findViewById(R.id.datePicker);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        this.datePicker.init(year, month, day, (datePicker, year1, month1, dayOfMonth) -> datePickerChange(datePicker, year1, month1, dayOfMonth));
+
+
         return view;
+    }
+
+    private void datePickerChange(DatePicker datePicker, int year, int month, int dayOfMonth) {
+//            Log.d("Date", "Year=" + year + " Month=" + (month + 1) + " day=" + dayOfMonth);
+        textViewNoteCreateData.setText(dayOfMonth + "-" + (month + 1) + "-" + year);
     }
 }
