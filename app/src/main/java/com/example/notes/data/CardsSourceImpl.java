89f394459803446c1380f79051f7bf4b2682bebd
Stepan <bai_777@mail.ru>
2021-07-07 11:35:10 +0300

    Создал interface CardsSourceImpl

--- app/src/main/java/com/example/notes/data/CardsSourceImpl.java
+++ app/src/main/java/com/example/notes/data/CardsSourceImpl.java
@@ -1,5 +1,55 @@
 package com.example.notes.data;
 
-public class CardsSourceImpl {
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 
+import com.example.notes.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardsSourceImpl implements CardsSource {
+
+    private List<CardData> dataSource;
+    private Resources resources; // ресурсы приложения
+
+    public CardsSourceImpl(Resources resources) {
+        dataSource = new ArrayList<>(7);
+        this.resources = resources;
+    }
+
+    public CardsSourceImpl init() {
+        // строки заголовков из ресурсов
+        String[] titles = resources.getStringArray(R.array.titles);
+        // строки описаний из ресурсов
+        String[] descriptions = resources.getStringArray(R.array.descriptions);
+        // изображения
+        int[] pictures = resources.getIntArray(R.array.pictures);
+        // заполнение источника данных
+        for (int i = 0; i < descriptions.length; i++) {
+            dataSource.add(new CardData(titles[i], descriptions[i], pictures[i], false));
+        }
+        return this;
+    }
+
+    // Механизм вытаскивания идентификаторов картинок
+    private int[] getImageArray(){
+        TypedArray pictures = resources.obtainTypedArray(R.array.pictures);
+        int length = pictures.length();
+        int[] answer = new int[length];
+        for (int i = 0; i < length; i++) {
+            answer[i] = pictures.getResourceId(i, 0);
+        }
+        return answer;
+    }
+
+    @Override
+    public CardData getCardData(int position) {
+        return dataSource.get(position);
+    }
+
+    @Override
+    public int size() {
+        return dataSource.size();
+    }
 }
